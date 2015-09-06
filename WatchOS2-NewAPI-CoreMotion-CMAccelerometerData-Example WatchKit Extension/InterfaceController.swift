7f75f98e1e2fbe085bbe7a2ddcde78ceaf18ee04
Wlad Dicario <wlad@2v-prod.com>
2015-09-07 01:57:05 +0200

    Initial Commit.

--- WatchOS2-NewAPI-CoreMotion-CMAccelerometerData-Example WatchKit Extension/InterfaceController.swift
+++ WatchOS2-NewAPI-CoreMotion-CMAccelerometerData-Example WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,86 @@
 
 import WatchKit
 import Foundation
-
+import CoreMotion
 
 class InterfaceController: WKInterfaceController {
+    
+    // MARK: - Interface
+    @IBOutlet var xLbl: WKInterfaceLabel!
+    @IBOutlet var yLbl: WKInterfaceLabel!
+    @IBOutlet var zLbl: WKInterfaceLabel!
+    @IBOutlet var xValues: WKInterfaceLabel!
+    @IBOutlet var yValues: WKInterfaceLabel!
+    @IBOutlet var zValues: WKInterfaceLabel!
 
+    // MARK: - Properties
+    private let motionManager = CMMotionManager()
+    
+    
+    // MARK: - Context Initializer
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        self.xValues.setText("-")
+        self.yValues.setText("-")
+        self.zValues.setText("-")
     }
 
+    
+    // MARK: - Calls
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        // set manager updates
+        getMotionManagerUpdates()
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        // to stop updates
+        self.motionManager.stopAccelerometerUpdates()
     }
 
+    
+    // MARK: - Get Accelerometer Data
+    func getMotionManagerUpdates() {
+        
+        // init interval for update (NSTimeInterval)
+        self.motionManager.accelerometerUpdateInterval = 0.1
+        
+        // get current accelerometerData
+        if self.motionManager.accelerometerAvailable {
+            
+            // operation main queue
+            let mainQueue: NSOperationQueue = NSOperationQueue.mainQueue()
+           
+            // start accelerometer updates
+            self.motionManager.startAccelerometerUpdatesToQueue(mainQueue, withHandler: { (accelerometerData:CMAccelerometerData?, error:NSError?) -> Void in
+                // errors
+                if (error != nil) {
+                    print("error: \(error?.localizedDescription)")
+                }else{
+                    // success
+                    if ((accelerometerData) != nil) {
+                        
+                        // get accelerations values
+                        let x:String = NSString(format: "%.2f", (accelerometerData?.acceleration.x)!) as String
+                        let y:String = NSString(format: "%.2f", (accelerometerData?.acceleration.y)!) as String
+                        let z:String = NSString(format: "%.2f", (accelerometerData?.acceleration.z)!) as String
+                        
+                        print("x: \(x)")
+                        print("y: \(y)")
+                        print("z: \(z)")
+                        
+                        // set text labels
+                        self.xValues.setText(x)
+                        self.yValues.setText(y)
+                        self.zValues.setText(z)
+                    }
+                }
+            })
+        }
+    }
 }
--- WatchOS2-NewAPI-CoreMotion-CMAccelerometerData-Example/ViewController.swift
+++ WatchOS2-NewAPI-CoreMotion-CMAccelerometerData-Example/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    // MARK: - Property
+    private let motionManager = CMMotionManager()
+    
+    
+    // MARK: - Calls
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getMotionManagerData()
     }
 
+    // MARK: - Memory
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: - Get Accelerometer Data
+    func getMotionManagerData() {
+    
+        self.motionManager.accelerometerUpdateInterval = 0.1
+        
+        if self.motionManager.accelerometerAvailable {
+            let mainQueue: NSOperationQueue = NSOperationQueue.mainQueue()
+            self.motionManager.startAccelerometerUpdatesToQueue(mainQueue, withHandler: { (accelerometerData:CMAccelerometerData?, error:NSError?) -> Void in
+                //
+                if (error != nil) {
+                    print("error: \(error?.localizedDescription)")
+                }else{
+                    if ((accelerometerData) != nil) {
+                        
+                        let x:String = NSString(format: "%.2f", (accelerometerData?.acceleration.x)!) as String
+                        let y:String = NSString(format: "%.2f", (accelerometerData?.acceleration.y)!) as String
+                        let z:String = NSString(format: "%.2f", (accelerometerData?.acceleration.z)!) as String
+                        print("x: \(x)")
+                        print("y: \(y)")
+                        print("z: \(z)")
+                    }
+                }
+            })
+        }
+    }
 
 }
 
